ae8f0531fc2b410bad7a68640011d5d34edf7eb4
Ashley Dunham <xenocide21@gmail.com>
2020-02-22 00:41:28 -0800

    added more classes and weapons

--- src/data/classes.js
+++ src/data/classes.js
@@ -8,12 +8,21 @@ import {
   LONGSWORD,
   RAPIER,
   SHORTSWORD,
-  DAGGERS,
-  DARTS,
-  SLINGS,
-  QUARTERSTAFFS,
+  DAGGER,
+  DART,
+  SLING,
+  QUARTERSTAFF,
   LIGHT_CROSSBOW,
+  SHIELD,
+  SHIELD_WOOD,
+  CLUB,
+  SPEAR,
+  SICKLE,
+  SCIMITAR,
+  JAVELIN,
+  MACE, HEAVY_ARMOR
 } from './equipament-types';
+
 import {STR, DEX, CON, CHA, INT, WIS} from './abilities';
 import * as skills from './skills';
 
@@ -24,6 +33,7 @@ export const BARBARIAN = recursiveFreeze({
     armor: [
       LIGHT_ARMOR,
       MEDIUM_ARMOR,
+      SHIELD,
     ],
     shield: true,
     weapons: [
@@ -70,25 +80,152 @@ export const BARD = recursiveFreeze({
       choices: skills,
       number: 3
     }
-  }
+  },
 });
-export const WIZARD = recursiveFreeze({
-  name: 'WIZARD',
-  hitDie: 6,
+export const CLERIC = recursiveFreeze({
+  name: 'CLERIC',
+  hitDie: 8,
   proficiencies: {
-    armor: [],
+    armor: [
+      LIGHT_ARMOR,
+      MEDIUM_ARMOR,
+      SHIELD
+    ],
     weapons: [
-      DAGGERS,
-      DARTS,
-      SLINGS,
-      QUARTERSTAFFS,
+      SIMPLE_WEAPON
+    ],
+    savingThrows: [
+      WIS,
+      CHA
+    ],
+    skills: {
+      choices: [
+        skills.HISTORY,
+        skills.INSIGHT,
+        skills.MEDICINE,
+        skills.PERSUASION,
+        skills.RELIGION,
+      ],
+      number: 2
+    }
+  }
+})
+export const DRUID = recursiveFreeze({
+  name: 'DRUID',
+  hitDie: 8,
+  proficiencies: {
+    armor: [
+        LIGHT_ARMOR,
+        MEDIUM_ARMOR,
+        SHIELD_WOOD,
+    ],
+    weapons: [
+      CLUB,
+      DAGGER,
+      DART,
+      JAVELIN,
+      MACE,
+      QUARTERSTAFF,
+      SCIMITAR,
+      SICKLE,
+      SLING,
+      SPEAR,
       LIGHT_CROSSBOW,
     ],
     savingThrows: [
       INT,
       WIS
     ],
-      // choices: skills,
+    skills: {
+      choices: [
+        skills.ARCANA,
+        skills.ANIMAL_HANDLING,
+        skills.INSIGHT,
+        skills.SURVIVAL,
+        skills.MEDICINE,
+        skills.RELIGION,
+        skills.NATURE,
+        skills.PERCEPTION,
+      ],
+      number: 2
+    }
+  }
+})
+export const FIGHTER = recursiveFreeze({
+  name: 'FIGHTER',
+  hitDie: 10,
+  proficiencies: {
+    armor: [
+        LIGHT_ARMOR,
+        MEDIUM_ARMOR,
+        HEAVY_ARMOR,
+        SHIELD
+    ],
+    weapons: [
+        SIMPLE_WEAPON,
+        MARTIAL_WEAPON
+    ],
+    savingThrows: [
+        STR,
+        CON
+    ],
+    skills: {
+      choices: [
+        skills.ACROBATICS,
+        skills.ANIMAL_HANDLING,
+        skills.ATHLETICS,
+        skills.HISTORY,
+        skills.INSIGHT,
+        skills.INTIMIDATION,
+        skills.PERCEPTION,
+        skills.SURVIVAL,
+      ],
+      number: 2
+    }
+  }
+})
+export const MONK = recursiveFreeze({
+  name: 'MONK',
+  hitDie: 8,
+  proficiencies: {
+    armor: [],
+    weapons: [
+      SIMPLE_WEAPON,
+      SHORTSWORD
+    ],
+    savingThrows: [
+      INT,
+      WIS
+    ],
+    skills: {
+      choices: [
+        skills.ARCANA,
+        skills.HISTORY,
+        skills.INSIGHT,
+        skills.INVESTIGATION,
+        skills.MEDICINE,
+        skills.RELIGION,
+      ],
+      number: 2
+    }
+  }
+})
+export const WIZARD = recursiveFreeze({
+  name: 'WIZARD',
+  hitDie: 6,
+  proficiencies: {
+    armor: [],
+    weapons: [
+      DAGGER,
+      DART,
+      SLING,
+      QUARTERSTAFF,
+      LIGHT_CROSSBOW,
+    ],
+    savingThrows: [
+      INT,
+      WIS
+    ],
       skills: {
         choices: [
           skills.ARCANA,
